c98342a2d443dc391ae79106d6162171a2e76367
nksoz <nata.ksovreli@gmail.com>
2024-05-06 19:23:58 +0400

    first commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+my_name = "nata"
+my_surname = "ksovreli"
+my_age = 15
+my_height = 156
+my_eyes = "brown"
+my_hair = "brown"
+my_sister_name = "mari"
+
+
+print(my_name, my_surname, my_age, my_height, my_eyes, my_hair, my_sister_name)
