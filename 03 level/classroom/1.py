d580ddcb9430287e2a36595814122a5a55b29c2f
nksoz <nata.ksovreli@gmail.com>
2024-05-27 17:53:49 +0400

    second commit

---
+++ 03 level/classroom/1.py
@@ -0,0 +1,5 @@
+# print(type("5"))
+print(10)
+print("5" + 5)
+print(5 + 5)
+#print("2" / 5)
---
+++ 03 level/homework/1.py
@@ -0,0 +1,28 @@
+# Task 4: 
+print( 5 + 3)       # Addition
+print( 8 - 2)    # Subtraction
+print( 4 * 6) # Multiplication
+print( 10 / 2)      # Division
+
+# Task 5: 
+# When combining text data, Python concatenates the strings, joining them together.
+
+text1 = "Hello, "
+text2 = "world!"
+print(text1 + text2)  # This will print "Hello, world!"
+
+# Task 7:
+# Creating a Python script to demonstrate various tasks
+# Task 4: Printing results of different arithmetic operations
+# Task 5: Combining two text data and printing the result
+# Task 6: Introducing a syntax error to observe how Python handles it
+
+# Task 8:
+variable1 = 10
+variable2 = 5
+print("Arithmetic operation:", variable1 * variable2)  # Multiplication of the two variables
+
+# Task 9: Explaining string and integer
+# String: A data type used to represent text, enclosed within single ('') or double ("") quotes.
+# Integer: A data type used to represent whole numbers without any decimal points.
+# In Python, strings are used for textual data, while integers are used for numerical calculations.
